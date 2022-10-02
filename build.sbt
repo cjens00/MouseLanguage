44670b5c16da727d1410d9462fee4eb190ca69ca
Cameron J <cjense36@msudenver.edu>
2022-10-02 10:45:53 -0600

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / scalaVersion := "2.13.9"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "MouseLanguage"
+  )
---
+++ src/main/scala/Lexeme.scala
@@ -0,0 +1,22 @@
+/*
+ * CS3210 - Principles of Programming Languages - Fall 2022
+ * Instructor: Thyago Mota
+ * Description: Prg 01 - Lexeme (a symbol with a token value)
+ * Student(s) Name(s): Cameron Jensen
+ */
+
+class Lexeme(var label: String, var token: Token.Value = null) {
+  override def toString: String = {
+    var s = s"Lexeme($label"
+    if (token != null)
+      s += s", $token"
+    s + ")"
+  }
+}
+
+object Lexeme {
+  def main(args: Array[String]): Unit = {
+    val lex = new Lexeme("statement", Token.IDENTIFIER)
+    println(lex)
+  }
+}
